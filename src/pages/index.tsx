6be85723a314d16f9762125f6aa8240e4bf93dbf
eldarcodes <prog.eldar@gmail.com>
2023-05-19 17:48:28 +0300

    feat(structure): prepare components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
 import Head from "next/head";
 
+import { styles } from "~/helpers/styles";
+import {
+  Business,
+  Hero,
+  Navbar,
+  Stats,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "~/components";
+
 import { type NextPage } from "next";
 
 const Home: NextPage = () => {
@@ -9,7 +23,32 @@ const Home: NextPage = () => {
         <title>Hoobank</title>
       </Head>
 
-      <div className="font-poppins">test</div>
+      <div className="w-full overflow-hidden bg-primary">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Stats />
+            <Business />
+            <Billing />
+            <CardDeal />
+            <Testimonials />
+            <Clients />
+            <CTA />
+            <Footer />
+          </div>
+        </div>
+      </div>
     </>
   );
 };

2ebf02b091aed6cf9c3fc80c24d33377b04fe618
eldarcodes <prog.eldar@gmail.com>
2023-05-19 17:30:04 +0300

    feat(global): initial assets, fonts, styles, etc.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
-import { type AppType } from "next/dist/shared/lib/utils";
+import { Poppins } from "next/font/google";
 
 import "~/styles/globals.css";
 
+import { type AppType } from "next/dist/shared/lib/utils";
+
+// If loading a variable font, you don't need to specify the font weight
+const poppins = Poppins({ weight: ["400", "700"], subsets: ["latin"] });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default MyApp;
